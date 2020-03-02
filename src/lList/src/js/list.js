e46569db0c4e4158d74dbd8dea0d859d72e99761
YyriyLaznyuk <yuriy.laznyuk@gmail.com>
2020-03-03 01:51:05 +0200

    llist

---
+++ src/lList/src/js/list.js
@@ -0,0 +1,21 @@
+
+module.exports.List=List;
+function List(array) {}
+List.prototype.init = function() {};
+List.prototype.getSize = function() {};
+List.prototype.addStart = function() {};
+List.prototype.addAnd = function() {};
+List.prototype.delStar = function() {};
+List.prototype.delEnd = function() {};
+List.prototype.delPos = function() {};
+List.prototype.getIndex = function() {};
+List.prototype.setIndex = function() {};
+List.prototype.myToString = function() {};
+List.prototype.Clear = function() {};
+List.prototype.getMin = function() {};
+List.prototype.getMax = function() {};
+List.prototype.sort = function() {};
+List.prototype.getMinIndex = function() {};
+List.prototype.getMaxIndex = function() {};
+List.prototype.getRevers = function() {};
+List.prototype.getHalfRevers = function() {};
---
+++ src/lList/src/js/node.js
@@ -0,0 +1,6 @@
+module.exports.Node=Node;
+
+function Node(data) {
+    this.data = data;
+    this.next = null;
+  }
